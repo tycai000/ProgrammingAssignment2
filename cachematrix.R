b9056eee28fa22a3565d75581bdd14f5e068b305
tycai000 <tycai000@gmail.com>
2014-12-16 16:18:01 +1100

    Added functions makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Two functions used to solve and cache matrices in order to speed up processing of matrix data
 
-## Write a short comment describing this function
-
+#Creates a special matrix which produces a list to set and get the matrix, set and get the solution of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) { #sets the matrix
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x #returns the value of original matrix
+  setinverse <- function(solve) s <<- solve # function used by cacheSolve in first instance to solve the original matrix
+  getinverse <- function() s #function used by cacheSolve in order to retrieve a cached matrix solution
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+#Input is a matrix created by makeCacheMatrix, output is its solution
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getinverse() #if matrix solution has been previous solved and cached, then use getinverse() function to retrieve result
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get() #if matrix solution has not been solved previously, then solve matrix,cache matrix solution, and then print
+  s <- solve(data, ...)
+  x$setinverse(s)
+  s
 }
